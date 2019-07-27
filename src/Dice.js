7af70df799aefe9e921354d41375f96fe75eeecf
{dp7jm} <{dpjm@chef.net}>
2019-07-27 18:14:29 +0200

    creted commponents

---
+++ src/Dice.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'rect';
+
+class Dice extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Dice;
